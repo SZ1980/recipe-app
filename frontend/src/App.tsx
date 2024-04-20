021c40f913644a219b585d89105277d299c6d435
Zoran Simonovic <simonovic1980@gmail.com>
2024-04-20 15:12:30 +0200

    connecting front with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,49 @@
-import './App.css'
+import { FormEvent, useState } from "react";
+//  useEffect, useRef,
+import * as api from "./api";
+import { Recipe } from "./types";
+import "./App.css";
 
 function App() {
-  
+  const [searchTerm, setSearchTerm] = useState<string>("burgers");
+  // useState<string>("");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  // const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(
+  //   undefined
+  // );
+  // const [selectedTab, setSelectedTab] = useState<Tabs>("search");
+  // const [favouriteRecipes, setFavouriteRecipes] = useState<Recipe[]>([]);
+  // const pageNumber = useRef(1);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+
+      // pageNumber.current = 1;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <>
-      <div>Hello from recipe app</div>
-            </>
-  )
+      <div>
+        <form onSubmit={(event) => handleSearchSubmit(event)}>
+          <button type="submit">submit</button>
+        </form>
+
+        {recipes.map((recipe) => (
+          <div>
+            recipe image location: {recipe.image}
+            recipe title: {recipe.title}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
